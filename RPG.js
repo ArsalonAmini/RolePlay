c4014f5716c8c9adc894b5c5e9989a108acda066
ArsalonAmini <aminihajibasa@gmail.com>
2016-09-26 12:30:39 -0500

    jiberish javascript

--- RPG.js
+++ RPG.js
@@ -1,2 +1,92 @@
 //this is the start of our group project//
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this is the Start of Arsalon's Obstacle OOP ///
+
+
+
+
+function Obstacle(){
+    Obstacle.prototype.attackPlayer = function(player){
+        return this.playerHealth;
+    };
+    
+    function DepleteHealth(player){
+        this.reduceHealth = function(){ var health = player.health - 5; return this.health; }
+    };
+    DepleteHealth.prototype = new Obstacle();
+
+
+var monster = Object.create(Obstacle);
+var Assassin = Object.create(Ob)
+
+Monster.prototype.AttactPlayer = function (player){
+    player.healthPoints = +-10;
+    return player;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Monster.constructor;
+Assassin.constructor;
+QuickSand.constructor;
