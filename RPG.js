91c58864a13d92f1a77106f7b54c1043a24a0aa7
ArsalonAmini <aminihajibasa@gmail.com>
2016-09-26 17:03:58 -0500

    deletions

--- RPG.js
+++ RPG.js
@@ -30,43 +30,10 @@ var ninja = Object.create(player);
 ninja.weapon = "Nunchucks";
 var cowboy = Object.create(player);
 cowboy.weapon = "Rifle";
-  
 alien.getTransport;
-
 
 }
 initiate();
 
 
-//this is the Start of Arsalon's Obstacle OOP ///
-function Obstacle (player){
-    this.player = player;
-}
-Obstacle.prototype.depleteHealth = function(player){
-    player.health -= 5;
-    return this.player;
-}
-Obstacle.prototype.giveGold = function(player){
-    player.gold += 1;
-    return this.player;
-}
-
-function Monstor(){
-    Monstor.prototype = object.create(Obstacle.prototype);
-}
-Monstor.prototype.depleteHealth -=10;
-Monstor.prototype.giveGold += 0; 
-
-function Assassin(){
-    Assassin.prototype = object.create(Obstacle.prototype);
-}
-Assassin.prototype.depleteHealth -=5;
-Assassin.prototype.giveGold += 0; 
-
-
-function Wizard(){
-    Wizard.prototype = object.create(Obstacle.prototype);
-}
-Wizard.prototype.depleteHealth =0;
-Wizard.prototype.giveGold += 10; 
 
