fb91485c01f693c0754cda886baa5d3ba658d66a
maga-senthil <magalakshmi.s@gmail.com>
2016-09-27 07:29:19 -0500

    inheritance

--- RPG.js
+++ RPG.js
@@ -1,34 +1,80 @@
 //this is the start of our group project//
-function player(name,weapon){
+function player(){
 	this.name = name;
-	this.gold = 0 ;
+	this.gold = 0;
 	this.health = 100;
-	this.weapon = weapon;
 }
+function characters(){
+	this.addGold = function(amount){
+		this.gold += amount;
+	}
+}	
 player.prototype.getName = function(){
 	return this.name;
-}
-player.prototype.addGold = function(amount){
-	this.gold += amount;
 }
 player.prototype.depleteHealth=function(amount){
 	this.health -= amount;
 }
-
 function initiate(){
-var alien = new player("Alien");
-alien.weapon = "Laser Pistol";
-var ninja = new player("Ninja");
+characters.prototype = new player();
+var alien = new characters();
+alien.name="Alien";
+alien.weapon="Laser Pistol";
+alien.addGold(10);
+alien.depleteHealth(2);
+var ninja = new characters();
+ninja.name = "Ninja";
 ninja.weapon = "Nunchucks";
-var cowboy = new player("Cowboy");
+ninja.addGold(20);
+ninja.depleteHealth(5);
+var cowboy = new characters();
+cowboy.name = "Cowboy";
 cowboy.weapon = "Rifle";
-
- alien.addGold(10);
- alien.depleteHealth(2);
- console.log(alien);
-alien.depleteHealth;
+cowboy.addGold(5);
+cowboy.depleteHealth(5);
+console.log(alien);
+console.log(ninja);
+console.log(cowboy);
+//alien.depleteHealth;
 
 }
+function choosePlayer(name){
+	if(name == "Alien")
+	{
+		alien.addGold(10);
+
+	}
+	if(name == "Ninja")
+	{
+		ninja.addGold(10);
+	}
+	if(name == "Cowboy")
+	{
+		cowboy.addGold(10);
+		alert(gold);
+	}
+	
+}
+function runGame(name){
+	switch(name){
+		case "Alien":
+			answer="";
+			break;
+		case "Ninja":
+			answer="";
+			break;
+		case "Cowboy":
+			answer="";
+			break;
+	}
+	return answer;
+}
+function print(){
+	var msg ="hello";
+	return msg;
+	//document.getElementById("message").innerHtml = "hello";
+}
+
 initiate();
 
 
